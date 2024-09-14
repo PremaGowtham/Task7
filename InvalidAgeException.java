0891ef8d8a76f69a1b60290a727c12b4d3b8d27a
PremaGowtham <premalathamegala@gmail.com>
2024-09-14 21:33:41 +0530

    InvalidAgeException

---
+++ InvalidAgeException.java
@@ -0,0 +1,11 @@
+package Task7;
+
+import java.util.Scanner;
+
+public class InvalidAgeException extends Exception {
+    // Constructor that accepts a custom message
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
